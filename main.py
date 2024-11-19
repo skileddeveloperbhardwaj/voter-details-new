12e3e54d9d5f44783411cad28ca33fdf65737fd7
skileddeveloperbhardwaj <skilled.developer.bhardwaj@gmail.com>
2024-11-19 12:15:38 +0530

    test

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-# import flet as flt 
-  
-# def myapp(page: flt.Page): 
-#     pass
-  
-  
-# flt.app(target=myapp)
-
 import flet as ft
-import pandas as pd
+# import pandas as pd
 
 def main(page: ft.Page):
     page.title = "Flet counter example"
@@ -24,14 +16,14 @@ def main(page: ft.Page):
         txt_number.value = str(int(txt_number.value) + 1)
         page.update()
 
-    def panda_check(e):
-        mydataset = {
-        'cars': ["BMW", "Volvo", "Ford"],
-        'passings': [3, 7, 2]
-        }
-        myvar = pd.DataFrame(mydataset)
-        txt_number.value = str(myvar)
-        page.update()
+    # def panda_check(e):
+    #     mydataset = {
+    #     'cars': ["BMW", "Volvo", "Ford"],
+    #     'passings': [3, 7, 2]
+    #     }
+    #     myvar = pd.DataFrame(mydataset)
+    #     txt_number.value = str(myvar)
+    #     page.update()
 
     page.add(
         ft.Row(
@@ -39,11 +31,14 @@ def main(page: ft.Page):
                 ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
                 txt_number,
                 ft.IconButton(ft.icons.ADD, on_click=plus_click),
-                ft.FilledButton(text="Filled button", on_click=panda_check),
+                # ft.FilledButton(text="Filled button", on_click=panda_check),
             ],
             alignment=ft.MainAxisAlignment.CENTER,
         )
     )
 
+    page.update()
+    ft.app(target=main)
 
-ft.app(main, view=ft.AppView.WEB_BROWSER)
+
+# ft.app(target=main, view=ft.AppView.WEB_BROWSER)
